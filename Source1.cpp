55a4f7b996a3698cbbf6a39f18147b94bd74f09b
24secondss <78076663+24secondss@users.noreply.github.com>
2021-02-03 11:16:49 +0100

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -2,8 +2,19 @@
 
 SOCKET socket(int af, int type, int protocol);
 
-sock = socket(AF_INET, SOCK_STREAM, 0);
-if (sock < 0)
+struct sockaddr_in {
+   sa_family sin_family;          // Adressfamilie AF_XXX
+   unsigned short int sin_port;   // Portnummer
+   struct in_addr sin_addr;       // IP-Adresse
+   unsigned char pad[8];          // Auffüllbytes für sockaddr
+ };
+
+client1 = socket(AF_INET, SOCK_STREAM, 0);
+if (client1 < 0)
 {
 	//Fehlermeldung
 }
+
+struct sockaddr_in 
+
+int connect (SOCKET client1, const struct sockaddr_in FAR* addr, sizeof(sockaddr_in));
