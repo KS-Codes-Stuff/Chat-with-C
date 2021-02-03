9d2101f1eb63a900c11a7eee095d4d0844d8596b
24secondss <78076663+24secondss@users.noreply.github.com>
2021-02-03 11:37:38 +0100

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,13 +1,11 @@
 #include <winsock.h>
+#inlcude <netdb.h>
+
+char ip_addr;
+printf("Bitte geben Sie die IP-Adresse ihres Chatpartners ein: ")
+scanf("%s", ipa_addr);
 
 SOCKET socket(int af, int type, int protocol);
-
-struct sockaddr_in {
-   sa_family sin_family;          // Adressfamilie AF_XXX
-   unsigned short int sin_port;   // Portnummer
-   struct in_addr sin_addr;       // IP-Adresse
-   unsigned char pad[8];          // Auffüllbytes für sockaddr
- };
 
 client1 = socket(AF_INET, SOCK_STREAM, 0);
 if (client1 < 0)
@@ -19,10 +17,10 @@ struct sockaddr_in client2;
 
 struct sockaddr_in client2 =
 {
-	AF_INET;
-	...
-	...
-	...
-}
+	AF_INET,
+	...								//Portnummer
+	inet_aton(ip_addr),				//IP-Adresse
+	...								//Auffüllbytes für sockaddr
+};
 
 int connect (SOCKET client1, const struct sockaddr_in FAR* addr, sizeof(sockaddr_in));
